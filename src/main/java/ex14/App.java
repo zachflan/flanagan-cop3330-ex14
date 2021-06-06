38ae9244b0739c7878a29a9bfca8406b10c2511b
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 12:59:35 -0400

    Commit 3

--- src/main/java/ex14/App.java
+++ src/main/java/ex14/App.java
@@ -19,15 +19,13 @@ public class App {
         System.out.print("What is the state? ");
         String stateS = state.nextLine();
 
-        if(stateS.equals("WI")){
-            double tax = 0.055 * orderAmountD;
-            double wiTotal = tax + orderAmountD;
-            System.out.format("The subtotal is $%.2f.\n", orderAmountD);
-            System.out.format("The tax is $%.2f.\n", tax);
-            System.out.format("The total is $%.2f.", wiTotal);
-        } else {
-            System.out.format("The total is $%.2f.", orderAmountD);
-        }
+        double tax = 0.055 * orderAmountD;
 
+        if(stateS.equals("WI")){
+            System.out.format("The subtotal is $%.2f.\n", orderAmountD);
+            orderAmountD = orderAmountD + tax;
+            System.out.format("The tax is $%.2f.\n", tax);
+        }
+        System.out.format("The total is $%.2f.", orderAmountD);
     }
 }
